53417f9a1e474ad9d2e365af0364114384fb6b70
Ignasi <ignasi@polygon.technology>
2025-01-27 16:21:19 +0100

    Small fix

--- tools/createSovereignGenesis/create-sovereign-genesis.ts
+++ tools/createSovereignGenesis/create-sovereign-genesis.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-await-in-loop, no-use-before-define, no-lonely-if */
 /* eslint-disable no-console, no-inner-declarations, no-undef, import/no-unresolved */
 // external dependencies
-import {expect} from "chai";
 import path = require("path");
 import fs = require("fs");
 
