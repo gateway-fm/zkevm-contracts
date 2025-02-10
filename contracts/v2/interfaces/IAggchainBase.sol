1f1599ccc7e7e02875a9aa8fe9d714791dd33967
Ignasi <ignasi@polygon.technology>
2025-02-10 10:36:30 +0100

    Refactor aggchain interfaces

--- contracts/v2/interfaces/IAggchainBase.sol
+++ contracts/v2/interfaces/IAggchainBase.sol
@@ -31,20 +31,5 @@ interface IAggchainBaseErrors {
 }
 
 interface IAggchainBase is IAggchainBaseErrors, IAggchainBaseEvents {
-    /**
-     * @notice Gets aggchain hash.
-     * @dev Each chain should properly manage its own aggchain hash.
-     * @param customChainData Custom chain data to build the consensus hash.
-     */
-    function getAggchainHash(
-        bytes calldata customChainData
-    ) external view returns (bytes32);
 
-    /**
-     * @notice Callback from the PolygonRollupManager to update the chain's state.
-     * @dev Each chain should properly manage its own state.
-     * @param data Custom chain data to update chain's state
-     */
-    function onVerifyPessimistic(bytes calldata data) external;
-    function initialize(bytes calldata initializeBytesCustomChain) external;
 }
