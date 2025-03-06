7bd1b43170510f5da3da04280144375f89da1af1
invocamanman <jesus.ligero@yahoo.es>
2025-03-06 19:12:27 +0100

    udpate deploy sovereign

--- tools/deploySovereignTest/deploySovereign.ts
+++ tools/deploySovereignTest/deploySovereign.ts
@@ -63,7 +63,9 @@ async function main() {
     console.log(
         `npx hardhat verify --constructor-args upgrade/arguments.js ${sovereignChainGlobalExitRootContract.target} --network ${process.env.HARDHAT_NETWORK}\n`
     );
-    console.log("Copy the following constructor arguments on: upgrade/arguments.js \n", [deployer.address]);
+    console.log("Copy the following constructor arguments on: upgrade/arguments.js \n", [
+        sovereignChainBridgeContract.target,
+    ]);
 
     // intialize the bridge
     await sovereignChainBridgeContract.initialize(
@@ -82,16 +84,22 @@ async function main() {
     const receiptDeployment = await (await sovereignChainGlobalExitRootContract.deploymentTransaction())?.wait();
 
     // insert some gers
-    const randomInsertedRoots = 2;
+    const simulateGERs = simulateGERWithEtherClaims(deployer.address);
+    const gerToInsert = [ethers.hexlify(ethers.randomBytes(32)), simulateGERs.computedGlobalExitRoot];
     const globalExitRoots: any[] = [];
 
     // simulate l1 info tree
     const height = 32;
-    const merkleTree = new MerkleTreeBridge(height);
-    for (let i = 0; i < randomInsertedRoots; i++) {
-        const ger = ethers.hexlify(ethers.randomBytes(32));
+    const merkleTreeL1InfoTree = new MerkleTreeBridge(height);
+
+    for (let i = 0; i < gerToInsert.length; i++) {
+        const ger = gerToInsert[i];
+
+        // insert GER
+        console.log("inserting GER: ", ger);
         await (await sovereignChainGlobalExitRootContract.insertGlobalExitRoot(ger)).wait();
 
+        // Simulate l1 info tree
         const block = await ethers.provider.getBlock("latest");
         globalExitRoots.push({
             globalExitRoot: ger,
@@ -99,32 +107,88 @@ async function main() {
             timestamp: block?.timestamp,
         });
         const leafValue = calculateGlobalExitRootLeaf(ger, block?.hash, block?.timestamp);
-        merkleTree.add(leafValue);
+        merkleTreeL1InfoTree.add(leafValue);
     }
 
     // compute proofs
-    for (let i = 0; i < randomInsertedRoots; i++) {
-        const proof = merkleTree.getProofTreeByIndex(i);
+    for (let i = 0; i < globalExitRoots.length; i++) {
+        const proof = merkleTreeL1InfoTree.getProofTreeByIndex(i);
         globalExitRoots[i].proof = proof;
     }
 
+    // Insert and remove a GER
+    const removedGERs: any[] = [];
+    for (let i = 0; i < 1; i++) {
+        const ger = ethers.hexlify(ethers.randomBytes(32));
+
+        // insert GER
+        console.log("inserting GER: ", ger);
+        await (await sovereignChainGlobalExitRootContract.insertGlobalExitRoot(ger)).wait();
+
+        // Simulate l1 info tree
+        const block = await ethers.provider.getBlock("latest");
+        globalExitRoots.push({
+            globalExitRoot: ger,
+            blockHash: block?.hash,
+            timestamp: block?.timestamp,
+        });
+
+        // Remove ger
+        console.log("removing GER: ", ger);
+        await (await sovereignChainGlobalExitRootContract.removeGlobalExitRoots([ger])).wait();
+        removedGERs.push(ger);
+    }
+
     // make a bridge transaction to udpate the local exit root
-    const amount = 1;
-    const receiptUpdateExitRoot = await (
+    console.log("making a bridge transaction to update the local exit root");
+    const amount = 10;
+    await (
         await sovereignChainBridgeContract.bridgeAsset(1, deployer.address, amount, ethers.ZeroAddress, true, "0x", {
             value: amount,
         })
     ).wait();
 
-    console.log("Random global exit roots:", globalExitRoots);
+    // make claim transaction
+    const claimedGlobalIndexes: any[] = [];
+
+    console.log("making a claim transaction");
+    for (let i = 0; i < simulateGERs.claims.length; i++) {
+        const claim = simulateGERs.claims[i];
+        await (
+            await sovereignChainBridgeContract.claimAsset(
+                claim.proofLocal,
+                claim.proofRollup,
+                claim.globalIndex,
+                claim.mainnetExitRoot,
+                claim.rollupRoot,
+                claim.originNetwork,
+                claim.tokenAddress,
+                claim.destinationNetwork,
+                claim.destinationAddress,
+                claim.amount,
+                claim.metadata
+            )
+        ).wait();
+
+        claimedGlobalIndexes.push(claim.globalIndex);
+    }
+
+    // claim undo
+    const undoClaimReceipt = await (
+        await sovereignChainBridgeContract.unsetMultipleClaims([simulateGERs.claims[0].globalIndex])
+    ).wait();
+    const unclaimedGlobalIndexes = [simulateGERs.claims[0].globalIndex];
 
     output.initialBlockNumber = receiptDeployment?.blockNumber;
-    output.finalBlockNumber = receiptUpdateExitRoot?.blockNumber;
+    output.finalBlockNumber = undoClaimReceipt?.blockNumber;
     output.gerSovereignAddress = sovereignChainGlobalExitRootContract.target;
     output.globalExitRoots = globalExitRoots;
     output.localExitRoot = await sovereignChainGlobalExitRootContract.lastRollupExitRoot();
-    output.l1InfoRoot = merkleTree.getRoot();
+    output.l1InfoRoot = merkleTreeL1InfoTree.getRoot();
     output.chainId = Number((await currentProvider.getNetwork()).chainId);
+    output.removedGERs = removedGERs;
+    output.claimedGlobalIndexes = claimedGlobalIndexes;
+    output.unclaimedGlobalIndexes = unclaimedGlobalIndexes;
 
     fs.writeFileSync(pathOutput, JSON.stringify(output, null, 1));
 }
@@ -147,3 +211,90 @@ Object.defineProperty(BigInt.prototype, "toJSON", {
         return () => String(this);
     },
 });
+
+function simulateGERWithEtherClaims(destinationAddress: any) {
+    const networkIDMainnet = 0;
+    const LEAF_TYPE_ASSET = 0;
+
+    // Add a claim leaf to rollup exit tree
+    const originNetwork = networkIDMainnet;
+    const tokenAddress = ethers.ZeroAddress; // ether
+    const amount = 1;
+    const destinationNetwork = networkIDMainnet;
+
+    const metadata = "0x"; // since is ether does not have metadata
+    const metadataHash = ethers.solidityPackedKeccak256(["bytes"], [metadata]);
+
+    // compute root merkle tree in Js
+    const height = 32;
+    const merkleTree = new MerkleTreeBridge(height);
+    const leafValue = getLeafValue(
+        LEAF_TYPE_ASSET,
+        originNetwork,
+        tokenAddress,
+        destinationNetwork,
+        destinationAddress,
+        amount,
+        metadataHash
+    );
+
+    // Add couple leafs
+    merkleTree.add(leafValue);
+    merkleTree.add(leafValue);
+
+    const rootJSRollup = merkleTree.getRoot();
+    const merkleTreeRollup = new MerkleTreeBridge(height);
+    merkleTreeRollup.add(rootJSRollup);
+    const rollupRoot = merkleTreeRollup.getRoot();
+
+    const mainnetExitRoot = ethers.ZeroHash;
+    const computedGlobalExitRoot = calculateGlobalExitRoot(mainnetExitRoot, rollupRoot);
+
+    const output = {} as any;
+
+    // Modified: Include mainnetExitRoot and rollupRoot in both claims
+    output.computedGlobalExitRoot = computedGlobalExitRoot;
+    output.claims = [
+        {
+            originNetwork,
+            tokenAddress,
+            amount,
+            destinationNetwork,
+            destinationAddress,
+            metadata,
+            proofLocal: merkleTree.getProofTreeByIndex(0),
+            proofRollup: merkleTreeRollup.getProofTreeByIndex(0),
+            globalIndex: computeGlobalIndex(0, 0, false),
+            mainnetExitRoot,
+            rollupRoot,
+        },
+        {
+            originNetwork,
+            tokenAddress,
+            amount,
+            destinationNetwork,
+            destinationAddress,
+            metadata,
+            proofLocal: merkleTree.getProofTreeByIndex(1),
+            proofRollup: merkleTreeRollup.getProofTreeByIndex(0),
+            globalIndex: computeGlobalIndex(1, 0, false),
+            mainnetExitRoot,
+            rollupRoot,
+        },
+    ];
+    return output;
+}
+
+function calculateGlobalExitRoot(mainnetExitRoot: any, rollupExitRoot: any) {
+    return ethers.solidityPackedKeccak256(["bytes32", "bytes32"], [mainnetExitRoot, rollupExitRoot]);
+}
+
+const _GLOBAL_INDEX_MAINNET_FLAG = 2n ** 64n;
+
+function computeGlobalIndex(indexLocal: any, indexRollup: any, isMainnet: Boolean) {
+    if (isMainnet === true) {
+        return BigInt(indexLocal) + _GLOBAL_INDEX_MAINNET_FLAG;
+    } else {
+        return BigInt(indexLocal) + BigInt(indexRollup) * 2n ** 32n;
+    }
+}
