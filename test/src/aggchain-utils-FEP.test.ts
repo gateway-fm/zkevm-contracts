b9b5ad8b076ecac263eac8a4911a6c49fb812711
Laia Soler <laia@polygon.technology>
2025-05-14 14:08:35 +0200

    update test FEP

--- test/src/aggchain-utils-FEP.test.ts
+++ test/src/aggchain-utils-FEP.test.ts
@@ -39,7 +39,7 @@ describe('Test vectors aggchain FEP utils', () => {
     for (let i = 0; i < aggchainInitBytesV0.length; i++) {
         it(`Check test-vectors encode initialize bytes aggchain version 0 ID=${i}`, async () => {
             const testVector = aggchainInitBytesV0[i].input;
-            const initBytesAggchainECDSAv0 = utilsFEP.encodeInitializeBytesAggchainFEPv0(
+            const initBytesAggchainFEPv0 = utilsFEP.encodeInitializeBytesAggchainFEPv0(
                 testVector.initParams,
                 testVector.useDefaultGateway,
                 testVector.initOwnedAggchainVKey,
@@ -53,14 +53,14 @@ describe('Test vectors aggchain FEP utils', () => {
             );
             if (update) {
                 aggchainInitBytesV0[i].output = {};
-                aggchainInitBytesV0[i].output.initBytesAggchainECDSAv0 = initBytesAggchainECDSAv0;
-                console.log(`WRITE: ${path.join(pathTestvectors, 'aggchain-initBytesv0.json')}`);
+                aggchainInitBytesV0[i].output.initBytesAggchainFEPv0 = initBytesAggchainFEPv0;
+                console.log(`WRITE: ${path.join(pathTestvectors, "aggchain-initBytesv0.json")}`);
                 fs.writeFileSync(
                     path.join(pathTestvectors, 'aggchain-initBytesv0.json'),
                     JSON.stringify(aggchainInitBytesV0, null, 2),
                 );
             } else {
-                expect(initBytesAggchainECDSAv0).to.equal(aggchainInitBytesV0[i].output.initBytesAggchainECDSAv0);
+                expect(initBytesAggchainFEPv0).to.equal(aggchainInitBytesV0[i].output.initBytesAggchainFEPv0);
             }
         });
     }
@@ -68,7 +68,7 @@ describe('Test vectors aggchain FEP utils', () => {
     for (let i = 0; i < aggchainInitBytesV1.length; i++) {
         it(`Check test-vectors encode initialize bytes aggchain version 1 ID=${i}`, async () => {
             const testVector = aggchainInitBytesV1[i].input;
-            const initBytesAggchainECDSAv1 = utilsFEP.encodeInitializeBytesAggchainFEPv1(
+            const initBytesAggchainFEPv1 = utilsFEP.encodeInitializeBytesAggchainFEPv1(
                 testVector.initParams,
                 testVector.useDefaultGateway,
                 testVector.initOwnedAggchainVKey,
@@ -77,14 +77,14 @@ describe('Test vectors aggchain FEP utils', () => {
             );
             if (update) {
                 aggchainInitBytesV1[i].output = {};
-                aggchainInitBytesV1[i].output.initBytesAggchainECDSAv1 = initBytesAggchainECDSAv1;
-                console.log(`WRITE: ${path.join(pathTestvectors, 'aggchain-initBytesv1.json')}`);
+                aggchainInitBytesV1[i].output.initBytesAggchainFEPv1 = initBytesAggchainFEPv1;
+                console.log(`WRITE: ${path.join(pathTestvectors, "aggchain-initBytesv1.json")}`);
                 fs.writeFileSync(
                     path.join(pathTestvectors, 'aggchain-initBytesv1.json'),
                     JSON.stringify(aggchainInitBytesV1, null, 2),
                 );
             } else {
-                expect(initBytesAggchainECDSAv1).to.equal(aggchainInitBytesV1[i].output.initBytesAggchainECDSAv1);
+                expect(initBytesAggchainFEPv1).to.equal(aggchainInitBytesV1[i].output.initBytesAggchainFEPv1);
             }
         });
     }
