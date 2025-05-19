2e32bcfd4e5790e8e66f5721a67ca379a74430b1
krlosMata <krlosmata@gmail.com>
2025-05-19 15:14:40 +0200

    custom defaults to local

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -318,7 +318,9 @@ const config: HardhatUserConfig = {
             },
         },
         custom: {
-            url: process.env.CUSTOM_PROVIDER,
+            url: process.env.CUSTOM_PROVIDER
+                ? process.env.CUSTOM_PROVIDER
+                : "http://127.0.0.1:8545",
             accounts: {
                 mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
                 path: "m/44'/60'/0'/0",
